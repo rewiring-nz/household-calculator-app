92a47e7130ab0f9b0820aca9a8d957181bd3ef0d
Jenny Sahng <6475953+jennynz@users.noreply.github.com>
2024-11-26 17:14:10 +1300

    feat: show/hide replacement cost details

--- src/components/HouseholdSavings/ResultBox.tsx
+++ src/components/HouseholdSavings/ResultBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Tooltip, Typography, useTheme } from '@mui/material';
 import { formatNZD } from 'src/shared/utils/formatters';
 
@@ -10,6 +10,7 @@ interface BulletPoint {
 interface ResultBoxState { 
     label: string;
     heading: string | number;
+    subheading?: string | number;
     bulletPoints?: BulletPoint[];
     paragraph?: string;
     linkText?: string;
@@ -18,8 +19,9 @@ interface ResultBoxState {
 }
 
   
-const ResultBox: React.FC<ResultBoxState> = ({ label, heading, bulletPoints, paragraph, children }) => {
+const ResultBox: React.FC<ResultBoxState> = ({ label, heading, subheading, bulletPoints, paragraph, children }) => {
     const theme = useTheme();
+    const [isExpanded, setIsExpanded] = useState(false);
     
 
     return (
@@ -37,35 +39,56 @@ const ResultBox: React.FC<ResultBoxState> = ({ label, heading, bulletPoints, par
                 }}>
                 {heading}
             </Typography>
-            {bulletPoints && (
-                <Box
-                    component="ul"
-                    sx={{
-                        listStyleType: 'disc',
-                        paddingLeft: '1.5rem',
-                        marginBottom: '1.5rem'
-                    }}
-                >
-                    {bulletPoints.map((point, i) => (
-                        <Typography
-                            key={i}
-                            component="li"
-                            variant="body2"
-                            sx={{
-                                lineHeight: '1.5rem' 
-                            }}
-                            >
-                            <span style={{ 
-                                fontWeight: 'bold',
-                                }}>
-                                {point.label}: 
-                            </span>
-
-                            {typeof point.value === "string" ? point.value : formatNZD(point.value, 0)}
-
+            {subheading && 
+                <Typography variant="body2">
+                    <span style={{ fontWeight: '600' }}>{subheading}&nbsp;</span>
+                    {bulletPoints && <Typography
+                        variant="body2"
+                        sx={{
+                            cursor: 'pointer',
+                            textDecorationLine: 'underline',
+                            display: 'inline-block'
+                        }}
+                        onClick={() => setIsExpanded(!isExpanded)}>
+                            {isExpanded ? '(hide details)' : '(show details)'}
                         </Typography>
-                    ))}
-                </Box>
+                    }
+                </Typography>
+            }
+            {bulletPoints && isExpanded && (
+                <>    
+                    <Box
+                        component="ul"
+                        sx={{
+                            listStyleType: 'disc',
+                            paddingLeft: '1.5rem',
+                            marginBottom: '0.8rem'
+                        }}
+                    >
+                        {bulletPoints.map((point, i) => (
+                            <Typography
+                                key={i}
+                                component="li"
+                                variant="body2"
+                                sx={{
+                                    lineHeight: '1.5rem' 
+                                }}
+                                >
+                                <span style={{ 
+                                    fontWeight: 'bold',
+                                    }}>
+                                    {point.label}: 
+                                </span>
+
+                                {typeof point.value === "string" ? point.value : formatNZD(point.value, 0)}
+
+                            </Typography>
+                        ))}
+                    </Box>
+                    <Typography variant="body2">
+                        This shows only the cost of electric alternatives, without comparing the cost of replacing with fossil fuel models as machines break. In reality, the cost of electrification is lower; it is just the difference between electric and fossil fuel counterparts.
+                    </Typography>
+                </>
             )}
 
             {paragraph && (
